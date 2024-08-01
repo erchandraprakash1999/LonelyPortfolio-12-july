409999556066c0688e50d452f74699b70b9bf29e
VinayKumar018 <kumarvinay15381@gmail.com>
2024-08-01 18:47:02 +0530

    about section updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,112 @@
-import React, { useEffect } from 'react'
-import { about } from '../data'
-import colors from 'tailwindcss/colors';
-import { Element } from 'react-scroll';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PortFolioActions from '../RequestServices/PortFolioActions';
-
+import colors from 'tailwindcss/colors';
 export default function About({ theme }) {
-  const { portfolio } = useSelector((store) => store)
+  const { portfolio } = useSelector((store) => store);
   const data = portfolio.about?.data;
   const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(PortFolioActions.aboutapi())
-  }, [])
+    dispatch(PortFolioActions.aboutapi());
+  }, [dispatch]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <Element name='about'> <div className="pb-16"></div> </Element>
-      <main >
-        <div id='about' className="flex flex-col lg:flex-row items-center gap-6 justify-center container mx-auto px-4 py-6 font-ChakraPetch">
-          <div className="w-full lg:w-[45%] ">
-            <img src={data?.image} alt="Woman on Phone" className="rounded" />
-          </div>
-          <div className="w-full lg:w-[60%] ">
-            <h1 className=" text-3xl md:text-5xl font-semibold mb-4 font-RobotoSlab " style={{ color: colors[theme][700] }}>{data?.title}</h1>
-            <p className="text-gray-600 mb-8 text-lg" dangerouslySetInnerHTML={{ __html: data?.description }}></p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              {data?.items.map((item, index) => {
-                return <div className='flex' key={index}>
-                  <div className=' px-2' style={{ color: colors[theme][600] }}>
-                    {item.icons}
-                  </div>
-                  <div className='text-gray-600'>
-                    <div className=" text-5xl font-bold  mb-2">{item.number}</div>
-                    <p ><strong>{item.text}</strong></p>
-                  </div>
-                </div>
-              })}
+    <section id="about">
+      <div className="bg-gray-100 p-6">
+        <div className="flex flex-wrap">
+          <div className="md:w-1/2 pl-15">
+            <h2
+              className="text-4xl font-bold pb-3 border-b-4 w-min font-RobotoSlab whitespace-nowrap text-black"
+              style={{ borderColor: colors[theme][800] }}
+            >
+             {data.title}
+            </h2>
+            <p className="pt-2 text-xl text-gray-600" >{data.description}</p>
+            <img src={data.image} alt="About" className="w-4/5 mt-4" />
+            <div className="pt-2 flex space-x-2">
+             <a href=""><i className="fa-brands fa-facebook p-3 rounded-full border-2  bg-white text-blue-700"    style={{ borderColor: colors[theme][500] }}></i></a> 
+             <a href=""> <i className="fa-brands fa-instagram p-3 rounded-full border-2 bg-white  text-rose-400"  style={{ borderColor: colors[theme][500] }}></i></a> 
+             <a href=""><i className="fa-brands fa-twitter p-3 rounded-full border-2  bg-white  text-blue-700"  style={{ borderColor: colors[theme][500] }}></i></a> 
+             <a href=""><i className="fa-brands fa-linkedin p-3 rounded-full border-2  bg-white  text-sky-500"  style={{ borderColor: colors[theme][500] }}></i></a> 
+             <a href=""><i className="fa-brands fa-github p-3 rounded-full border-2   bg-white  text-black"  style={{ borderColor: colors[theme][500] }}></i></a> 
             </div>
           </div>
+          <div className="md:w-1/2 mt-20">
+            <h1 className="text-3xl font-bold pb-3 w-min font-RobotoSlab whitespace-nowrap text-black">{data.about_information?.title || 'I am Vinay Kumar'}</h1>
+            <h6 className="text-lg mt-0 font-RobotoSlab text-gray-600">
+              A Lead <span className="text-gradient ">Stack & Mern Stack Developer</span> based in <span className="text-gradient">INDIA</span>
+            </h6>
+            <p className="text-sm mt-4 font-RobotoSlab text-gray-600">
+              As a Creative Software Developer, I combine my technical expertise with a deep understanding of design principles to create engaging digital solutions. My services include:
+            </p>
+            <div className="flex flex-wrap mt-4">
+              <div className="md:w-1/2 p-3">
+                <table className="table-auto w-full border-collapse border">
+                  <tbody>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Birthday</td>
+                      <td className="p-2 ">{data.about_information?.birthday || '18 July 2002'} </td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Age</td>
+                      <td className='p-2'>{data.about_information?.age || '22 Yr'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Residence</td>
+                      <td className="p-2">{data.about_information?.residence || 'INDIA'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Address</td>
+                      <td className="p-2">{data.about_information?.address || 'Lucknow Uttar Pradesh, INDIA'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Freelance</td>
+                      <td className="p-2">{data.about_information?.freelance || 'Available'}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              
+              </div>
+              <div className="md:w-1/2 p-3">
+                <table className="table-auto w-full border-collapse border">
+                  <tbody>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">E-mail</td>
+                      <td className="p-2">{data.about_information?.email || 'vinay15381@gmail.com'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Phone</td>
+                      <td className="p-2">{data.about_information?.phone || '+91 9336713280'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Skype</td>
+                      <td className="p-2">{data.about_information?.skype || 'vinay15381'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Experience</td>
+                      <td className="p-2">{data.about_information?.experience || '1+ years'}</td>
+                    </tr>
+                    <tr className="border-b p-4">
+                      <td className="font-semibold border-r pr-2 p-2">Hourly Rate</td>
+                      <td className="p-2">{data.about_information?.hourlyRate || '4$'}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                
+              </div>
+              <a href=""><button className="mt-4 text-white py-2 px-4 rounded font-RobotoSlab font-bold" style={{backgroundColor:colors[theme][400]}}> Browse Resume</button></a>
+                 
+                
+            </div>
+
+          </div>
         </div>
-      </main>
-    </>
-  )
+      </div>
+    </section>
+  );
 }
